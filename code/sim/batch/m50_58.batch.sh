7bce0fbf8ad9ccc0139ff33d36624ed7caa697b5
Michael Landis <mlandis@gmail.com>
2016-02-04 14:55:00 -0500

    rm more old batch files

--- code/sim/batch/m50_58.batch.sh
+++
@@ -1,5 +0,0 @@
-/Users/mlandis/Documents/code/revbayes-proj/Build/Products/Debug/RevLanguage ./sim/batch/m50_58.F0A0.inf.Rev > /dev/null &
-/Users/mlandis/Documents/code/revbayes-proj/Build/Products/Debug/RevLanguage ./sim/batch/m50_58.F0A1.inf.Rev > /dev/null &
-/Users/mlandis/Documents/code/revbayes-proj/Build/Products/Debug/RevLanguage ./sim/batch/m50_58.F1A0.inf.Rev > /dev/null &
-/Users/mlandis/Documents/code/revbayes-proj/Build/Products/Debug/RevLanguage ./sim/batch/m50_58.F1A1.inf.Rev > /dev/null &
-/Users/mlandis/Documents/code/revbayes-proj/Build/Products/Debug/RevLanguage ./sim/batch/m50_58.F0A0.prior.inf.Rev > /dev/null &
